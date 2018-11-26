0d117414dd632809eb0dbe960f6bbefe4ad656f4
Philippe Coval <p.coval@samsung.com>
2018-11-26 18:44:42 +0100

    moziot: Add activitypub-adapter as example
    Change-Id: I7c2eebf1f4df73933f14f568fa6eccc450239573
    Signed-off-by: Philippe Coval <p.coval@samsung.com>

---
+++ example/mozilla-iot-activitypub-adapter/index.js
@@ -0,0 +1,16 @@
+// -*- mode: js; js-indent-level:2;  -*-
+// SPDX-License-Identifier: MPL-2.0
+/**
+ * index.js - Loads the ActivityPub adapter.
+ *
+ * Copyright 2018-present Samsung Electronics Co., Ltd. and other contributors
+ */
+
+'use strict';
+
+function maybeLoadActivityPubAdapter(addonManager, manifest, errorCallback) {
+  const loadActivityPubAdapter = require('./activitypub-adapter');
+  return loadActivityPubAdapter(addonManager, manifest, errorCallback);
+}
+
+module.exports = maybeLoadActivityPubAdapter;
