aff923808542e0cd27eb2bcdec4884a2adeacf0e
Philippe Coval <rzr@users.sf.net>
2020-01-21 22:57:58 +0100

    moziot: Add manifest.json to checksums
    Change-Id: Iaed2b629512f2463bab800bd50ba9305de6abbf4
    Relate-to: https://github.com/rzr/mastodon-lite/issues/12
    Signed-off-by: Philippe Coval <rzr@users.sf.net>

--- example/mozilla-iot-activitypub-adapter/package.sh
+++ example/mozilla-iot-activitypub-adapter/package.sh
@@ -15,7 +15,7 @@ fi
 yarn install --production
 
 rm -f SHA256SUMS
-sha256sum package.json *.js LICENSE > SHA256SUMS
+sha256sum package.json *.js LICENSE manifest.json README.md > SHA256SUMS
 find node_modules -type f -exec sha256sum {} \; >> SHA256SUMS
 TARFILE="$(npm pack)"
 tar xzf ${TARFILE}
